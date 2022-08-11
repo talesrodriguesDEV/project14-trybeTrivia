8f15c466965b5d91831a37ea8a831015606456f0
Tales Rodrigues <tales00001@gmail.com>
2022-08-11 11:54:45 -0300

    Updates

---
+++ precache-manifest.8414b639bb620271e562f51dddbda1d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f34ec4f7c8509c4d05b5419d619b7af",
+    "url": "/project14-trybeTrivia/index.html"
+  },
+  {
+    "revision": "eb8717b6973476f3a700",
+    "url": "/project14-trybeTrivia/static/css/main.67ba8aa2.chunk.css"
+  },
+  {
+    "revision": "229e880fdeca51efbe1b",
+    "url": "/project14-trybeTrivia/static/js/2.66211431.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/project14-trybeTrivia/static/js/2.66211431.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb8717b6973476f3a700",
+    "url": "/project14-trybeTrivia/static/js/main.351ef6e1.chunk.js"
+  },
+  {
+    "revision": "8d9118444247665ea7b0",
+    "url": "/project14-trybeTrivia/static/js/runtime-main.3bb98ae7.js"
+  }
+]);
